b710de183a963c2c71405098c4415e7dc10ffbbc
JudeSahabandu <jude.sahabandu@gmail.com>
2022-11-30 17:49:52 -0500

    Updates

---
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -0,0 +1,40 @@
+// We create the tile layer that will be the background of our map.
+let lights = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// We create the dark view tile layer that will be an option for our map.
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Create a base layer that holds both maps.
+let baseMaps = {
+  Light: lights,
+  Dark: dark
+};
+
+
+// Create the map object with a center and zoom level.
+let map = L.map("mapid", {
+  center: [44.0, -80.0],
+  zoom: 2
+});
+
+// Then we add our 'graymap' tile layer to the map.
+L.control.layers(baseMaps).addTo(map);
+
+// Accessing the airport GeoJSON URL
+let torontoData = "https://raw.githubusercontent.com/JudeSahabandu/Mapping_Earthquakes/blob/main/Mapping_GeoJSON_Points/majorAirports.json";
+
+// Grabbing our GeoJSON data.
+// Grabbing our GeoJSON data.
+d3.json(torontoData).then(function(data) {
+  console.log(data);
+// Creating a GeoJSON layer with the retrieved data.
+L.geoJSON(data).addTo(map);
+});
